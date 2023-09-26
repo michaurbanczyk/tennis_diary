fc33de53116583c584404b6be60168b05e678bfc
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2023-09-26 20:01:27 +0200

    directory refactor

--- tennis_diary/urls.py
+++ tennis_diary/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tennis_diary project.
+URL configuration for tennis_diary2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
